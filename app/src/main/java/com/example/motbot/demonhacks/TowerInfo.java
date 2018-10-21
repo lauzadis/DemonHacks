4dcfb34cee0fa4bd8dac02a97676ed379bd7a208
Ridas Jagelavicius <RidasJagelavicius@gmail.com>
2018-10-21 12:03:42 -0500

    Revised Tower Info to add a tower, dude, and weapon. Also added an option to cycle weapons to illustrate proof of concept of the game.

--- app/src/main/java/com/example/motbot/demonhacks/TowerInfo.java
+++ app/src/main/java/com/example/motbot/demonhacks/TowerInfo.java
@@ -4,20 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class TowerInfo extends AppCompatActivity {
+    int cycleCounter = 1; //will be used to demonstrate proof of concept that weapons can be upgraded
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tower_info);
         final Tower userTower = (Tower) getIntent().getExtras().get("tower");
+
         //Create all our labels and bars
         TextView healthLabel = (TextView)findViewById(R.id.healthLabel);
         TextView timeSurvivedProgress = (TextView)findViewById(R.id.timeSurvivedProgress);
         ProgressBar towerHealthBar = (ProgressBar)findViewById(R.id.towerHealthBar);
+        final ImageView gunImage = (ImageView)findViewById((R.id.gunImage));
+
         //Creates Action Buttons to create actions for repair and upgrade
         Button upgrade = (Button)findViewById(R.id.upgrade);
         upgrade.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +38,8 @@ public class TowerInfo extends AppCompatActivity {
                 userTower.repair();
             }
         });
+        Button devGunCycle = (Button)findViewById(R.id.devGunCycle);
+
 
         //Setup progressbar values
         towerHealthBar.setMax(userTower.getMax_health());
@@ -41,5 +48,35 @@ public class TowerInfo extends AppCompatActivity {
         //Setup label texts
         healthLabel.setText("Current Health : " + userTower.getHealth() + "/ " + userTower.getMax_health());
         timeSurvivedProgress.setText(userTower.getTimeSurvived()/(60*60) + " Days");
+
+            devGunCycle.setOnClickListener(new View.OnClickListener() {
+                @Override
+            public void onClick(View v) {
+                    switch(cycleCounter){
+                        case (1):
+                            gunImage.setImageResource(R.drawable.gun1);
+                            cycleCounter++;
+                            break;
+                        case (2):
+                            gunImage.setImageResource(R.drawable.gun2);
+                            cycleCounter++;
+                            break;
+                        case (3):
+                            gunImage.setImageResource(R.drawable.gun3);
+                            cycleCounter++;
+                            break;
+                        case (4):
+                            gunImage.setImageResource(R.drawable.gun4);
+                            cycleCounter++;
+                            break;
+                        case (5):
+                            gunImage.setImageResource(R.drawable.gun5);
+                            cycleCounter++;
+                            break;
+                        case(6):
+                            cycleCounter = 1;
+
+                    }
+                }
     }
-}
+        );}}
